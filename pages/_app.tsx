3f89b8ce44f535f8fa4a68c56cfacc9281587115
ThomasPalamara <palamara.thomas@gmail.com>
2023-06-18 23:32:53 +0100

    add page title and footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,13 @@ import { IKContext } from 'imagekitio-react';
 import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
 import Nav from '../components/Navigation/Nav';
+import Footer from '../components/Navigation/Footer';
 import { PhotoContextProvider } from '../components/Contexts/PhotoContext';
 import { GetStaticProps } from 'next';
 import ImageKit from 'imagekit';
 import { PopupContextProvider } from '../components/Contexts/PopupContext';
+import PageTitle from '../components/Navigation/PageTitle';
+import { useRouter } from 'next/router';
 
 interface Props extends AppProps {
   props: {
@@ -14,8 +17,15 @@ interface Props extends AppProps {
   };
 }
 function MyApp({ Component, pageProps, props: { results } }: Props) {
-  const navHeight = 100;
-  const footerHeight = 50;
+  const navHeight = 120;
+  const footerHeight = 75;
+  console.log('pageProps :', pageProps);
+
+  const router = useRouter();
+  console.log('router :', router);
+  const arrPath = router.asPath.split('/');
+  const pageName = arrPath[arrPath.length - 1].replace('-', ' ');
+  console.log('pageName :', pageName);
 
   return (
     <IKContext
@@ -25,16 +35,17 @@ function MyApp({ Component, pageProps, props: { results } }: Props) {
       authenticationEndpoint="http://www.yourserver.com/auth"
     >
       <PopupContextProvider>
-        <div className="body h-full min-h-screen px-10">
+        <div className="body h-full min-h-screen px-16">
           <Nav height={navHeight} />
+          <PageTitle title={pageName === '' ? 'home' : pageName} />
           <div
-            className="w-full p-0"
-            // style={{ height: `calc(100vh - ${navHeight + footerHeight}px)` }}
+            className="w-full p-0 m-0 overflow-x-hidden overflow-y-scroll"
+            style={{ height: `calc(99vh - ${navHeight + footerHeight}px)` }}
           >
             <Component {...pageProps} />
           </div>
 
-          <Nav height={footerHeight} />
+          <Footer height={footerHeight} />
         </div>
       </PopupContextProvider>
     </IKContext>
