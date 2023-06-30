bdd05d60247d002c718e1cf5897cc82022d39ae2
ThomasPalamara <palamara.thomas@gmail.com>
2023-06-30 09:37:25 +0100

    fix hero cover

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,32 +1,20 @@
 import Image from 'next/image';
 import React from 'react';
 import ImageContainer from '../ImageContainer';
+import { IKImage } from 'imagekitio-react';
 
 const Hero: React.FC = () => {
   const name = 'Thomas Palamara';
   const titleClasses = 'uppercase font-semibold letter-wide';
   return (
-    <ImageContainer src="https://ik.imagekit.io/uiw3np2kr8ww/landscape/calanques.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1632172042339" />
+    <div
+      className="h-full w-full bg-center bg-cover"
+      style={{
+        backgroundImage:
+          "url('https://ik.imagekit.io/uiw3np2kr8ww/DSC02174__Mmg8D0-W.jpg?updatedAt=1674689620421&ik-sdk-version=react-1.0.10')",
+      }}
+    />
   );
 };
 
 export default Hero;
-
-{
-  /* <div className="hero min-h-screen h-screen bg-left-bottom bg-cover">
-<div className="pt-20 mb-20 flex items-center text-center h-full">
-  <div className="relative ml-5 w-1/2">
-    <span
-      className={`${titleClasses} text-9xl position absolute inset-x-0 opacity-30 text-transparent transform rotate-180`}
-      style={{
-        WebkitTextStroke: '1px rgba(255, 255, 255, 0.15)',
-        top: '-150%',
-      }}
-    >
-      {name}
-    </span>
-    <h1 className={`${titleClasses} text-6xl`}>{name}</h1>
-  </div>
-</div>
-</div> */
-}
